4e82416567c2396a0409f3527354a38bc13fd50d
michael <michaelrantohi@gmail.com>
2025-05-04 23:45:03 -0400

    Added Main function which will have the UI and ComplianceChecker. Created method for transforming CSV into dictionary to easily verufy GPO compliances

--- src/ComplianceChecker.py
+++ src/ComplianceChecker.py
@@ -1,3 +1,17 @@
+import csv
+
+
 class ComplianceChecker:
-    def __init__(self, gpo):
-        self.gpo = gpo
+
+    def create_NIST800_baseline(self, baseline_csv):
+        settings_and_values = {}
+
+        line = 0
+        with open(baseline_csv, 'r') as f:
+            for eachLine in f.readlines():
+                if line > 0:
+                    formatted = eachLine.split(",")
+                    settings_and_values[formatted[0]] = formatted[1]
+                line += 1
+        print("Setting:", settings_and_values.keys(), settings_and_values.values())
+
---
+++ src/Main.py
@@ -0,0 +1,6 @@
+from ComplianceChecker import ComplianceChecker
+
+cc = ComplianceChecker()
+
+cc.create_NIST800_baseline("../data/compliance_baseline.csv")
+
