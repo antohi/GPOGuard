686fe4b58ccb2e7ad111f6bb2d682267a89b89f4
michael <michaelrantohi@gmail.com>
2025-05-06 12:03:03 -0400

    Implemented baseline and GPO settings comparison with compliance check output, fixed newline handling

--- src/ComplianceChecker.py
+++ src/ComplianceChecker.py
@@ -1,9 +1,9 @@
 import csv
 
-
 class ComplianceChecker:
     def __init__(self):
-        self.settings_and_values = {}
+        self.bl_settings_and_values = {}
+        self.gpo_settings_and_values = {}
 
     def get_settings_and_values(self, baseline_csv):
         set_and_val = {}
@@ -11,9 +11,29 @@ class ComplianceChecker:
         with open(baseline_csv, 'r') as f:
             for eachLine in f.readlines():
                 if line > 0:
-                    formatted = eachLine.split(",")
+                    formatted = eachLine.strip().split(",")
                     set_and_val[formatted[0]] = formatted[1]
                 line += 1
         self.settings_and_values = set_and_val
+
+    def get_gpo_setings_and_values(self, gpo):
+        gpo_set_and_val = {}
+        with open(gpo, 'r') as f:
+            for eachLine in f.readlines():
+                if " = " in eachLine:
+                    formatted = eachLine.strip().split(" = ")
+                    gpo_set_and_val[formatted[0]] = formatted[1]
+
+        self.gpo_settings_and_values = gpo_set_and_val
+
+
+    def check_gpo_compliance(self):
+        for setting in self.gpo_settings_and_values.keys():
+            if setting in self.settings_and_values.keys():
+                if self.settings_and_values[setting] == self.gpo_settings_and_values[setting]:
+                    print(f"{setting}: COMPLIANT")
+                else:
+                    print(f"{setting}: NOT COMPLIANT")
+
 
 
--- src/Main.py
+++ src/Main.py
@@ -3,4 +3,7 @@ from ComplianceChecker import ComplianceChecker
 cc = ComplianceChecker()
 
 cc.get_settings_and_values("../data/compliance_baseline.csv")
+cc.get_gpo_setings_and_values("../data/lab_policy_compliant.txt")
+
+cc.check_gpo_compliance()
 
