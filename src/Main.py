8d670ba912dfce8f4d96ad68a953bd28d912bf8e
michael <michaelrantohi@gmail.com>
2025-05-06 19:23:49 -0400

    Added loop where uses can keep checking files until deciding to exit

--- src/Main.py
+++ src/Main.py
@@ -6,27 +6,35 @@ def list(folder):
 exit = False
 cc = ComplianceChecker()
 
-print("===[GPOGuard]===\n")
+print("===[GPOGuard]===")
 while exit == False:
-    print("=[MENU]=")
+    print("\n=[MENU]=")
     print("[1] GPO Compliance Checker\n")
     choice = input("> ")
 
     if choice == "1":
-        print("\nPlease select a GPO file to check for compliance (/data directory)"
-              "\nFiles in /data directory:")
-        print(list("../data"))
-        gpo_file = input("> ")
+        post_result_choice = "1"
+        while post_result_choice == "1":
+            print("\n=[GPO Compliance Checker]=")
+            print("[FILE SELECTION]")
+            print("Please select a GPO file to check for compliance (/data directory)"
+                  "\n\nFiles in /data directory:")
+            print(list("../data"))
+            gpo_file = input("> ")
 
-        print("\nPlease select a compliance baseline (/data directory)"
-              "\nFiles in /data directory:")
-        print(list("../data"))
-        baseline_file = input("> ")
-        cc.get_bl_settings_and_values(f"../data/{baseline_file}")
-        cc.get_gpo_settings_and_values(f"../data/{gpo_file}")
+            print("\nPlease select a compliance baseline (/data directory)"
+                  "\nFiles in /data directory:")
+            print(list("../data"))
+            baseline_file = input("> ")
+            cc.get_bl_settings_and_values(f"../data/{baseline_file}")
+            cc.get_gpo_settings_and_values(f"../data/{gpo_file}")
 
-        cc.check_gpo_compliance()
+            print("\n[COMPLIANCE RESULTS]")
+            cc.check_gpo_compliance()
+            print("\n[1] Check another file for compliance")
+            print("[2] Main Menu")
 
+            post_result_choice = input("> ")
 
     else:
         print("[!] INVALID MENU OPTION\n")
