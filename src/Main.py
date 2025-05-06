2887dd42d42aad9cd71ea3451f49a248ccb49b85
michael <michaelrantohi@gmail.com>
2025-05-06 19:09:03 -0400

    Added UI to display contents of folder for picking gpo file

--- src/Main.py
+++ src/Main.py
@@ -1,4 +1,29 @@
 from ComplianceChecker import ComplianceChecker
+import os
+def list(folder):
+    return [f for f in os.listdir(folder) if os.path.isfile(os.path.join(folder, f))]
+
+exit = False
+
+while exit == False:
+    print("===[GPOGuard]===\n")
+    print("=[MENU]=\n")
+    print("[1] GPO Compliance Checker\n")
+    choice = input("> ")
+
+    if choice == "1":
+        print("Please select a GPO file to check for compliance (/data directory)"
+              "\nFiles in /data directory:")
+        print(list("../data"))
+        choice = input("> ")
+
+    else:
+        print("[!] INVALID MENU OPTION\n")
+
+
+
+
+"""
 
 cc = ComplianceChecker()
 
@@ -7,3 +32,4 @@ cc.get_gpo_settings_and_values("../data/lab_policy_compliant.txt")
 
 cc.check_gpo_compliance()
 
+"""
