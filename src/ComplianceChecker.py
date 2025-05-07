aa9667b0044d6f7fe268a415ea8432a6cef7ca4c
michael <michaelrantohi@gmail.com>
2025-05-07 17:18:46 -0400

    Added error handling for when attempting to read baseline and gpo files, also for when trying to log scan results

--- src/ComplianceChecker.py
+++ src/ComplianceChecker.py
@@ -1,4 +1,5 @@
 import csv
+import logging
 from logging import info
 import json
 
@@ -14,23 +15,29 @@ class ComplianceChecker:
 
     # Retrieves settings and information from baseline file
     def get_bl_settings_and_values(self, baseline_csv):
-        with open(baseline_csv, mode='r') as b:
-            reader = csv.DictReader(b)
-            for row in reader:
-                setting = row['SettingName'].strip()
-                info = [row['ExpectedValue'].strip(), row['Framework'].strip(), row['ControlID'].strip(), row['Description'].strip(), row['Severity'].strip(), row['Category'].strip()]
-                self.bl_settings_and_values[setting] = info
+        try:
+            with open(baseline_csv, mode='r') as b:
+                reader = csv.DictReader(b)
+                for row in reader:
+                    setting = row['SettingName'].strip()
+                    info = [row['ExpectedValue'].strip(), row['Framework'].strip(), row['ControlID'].strip(), row['Description'].strip(), row['Severity'].strip(), row['Category'].strip()]
+                    self.bl_settings_and_values[setting] = info
+        except Exception as e:
+            print(f"[!] ERROR: Unable to read baseline file. Exception: {e}")
 
     # Retrieves settings and information from GPO file
     def get_gpo_settings_and_values(self, gpo):
         gpo_set_and_val = {}
-        with open(gpo, 'r') as f:
-            for eachLine in f.readlines():
-                if " = " in eachLine:
-                    formatted = eachLine.strip().split(" = ")
-                    gpo_set_and_val[formatted[0]] = formatted[1]
+        try:
+            with open(gpo, 'r') as f:
+                for eachLine in f.readlines():
+                    if " = " in eachLine:
+                        formatted = eachLine.strip().split(" = ")
+                        gpo_set_and_val[formatted[0]] = formatted[1]
 
-        self.gpo_settings_and_values = gpo_set_and_val
+            self.gpo_settings_and_values = gpo_set_and_val
+        except Exception as e:
+            print(f"[!] ERROR: Unable to read GPO file. Exception: {e}")
 
     # Checks compliance between baseline and gpo files
     def check_gpo_compliance(self):
@@ -61,23 +68,30 @@ class ComplianceChecker:
 
     # Logs compliance check results to csv doc
     def log_results(self):
-        csv_path = "../reports/compliance_report.csv"
-        json_path = "../reports/compliance_report.json"
-
-        # CSV
-        with open(csv_path, 'w') as f:
-            f.write(
-                "Baseline,SettingName,ExpectedValue,ActualValue,Framework,ControlID,Description,Severity,Category\n")
-            for rec in self.output_results:
+        try:
+            csv_path = "../reports/compliance_report.csv"
+            # CSV
+            with open(csv_path, 'w') as f:
                 f.write(
-                    f"{rec['baseline']},{rec['setting']},"
-                    f"{rec['expected']},{rec['actual']},"
-                    f"{rec['framework']},{rec['control_id']},"
-                    f"\"{rec['description']}\",{rec['severity']},{rec['category']}\n"
-                )
+                    "Baseline,SettingName,ExpectedValue,ActualValue,Framework,ControlID,Description,Severity,Category\n")
+                for rec in self.output_results:
+                    f.write(
+                        f"{rec['baseline']},{rec['setting']},"
+                        f"{rec['expected']},{rec['actual']},"
+                        f"{rec['framework']},{rec['control_id']},"
+                        f"\"{rec['description']}\",{rec['severity']},{rec['category']}\n"
+                    )
+        except Exception as e:
+            print(f"[!] ERROR: Unable to write report CSV file. Exception: {e}")
+            logging.error(f"[!] ERROR: Unable to write report CSV file. Exception: {e}")
 
-        # JSON
-        with open(json_path, 'w') as f:
-            json.dump(self.output_results, f, indent=2)
+        try:
+            # JSON
+            json_path = "../reports/compliance_report.json"
+            with open(json_path, 'w') as f:
+                json.dump(self.output_results, f, indent=2)
+        except Exception as e:
+            print(f"[!] ERROR: Unable to write report JSON file. Exception: {e}")
+            logging.error(f"[!] ERROR: Unable to write report JSON file. Exception: {e}")
 
 
