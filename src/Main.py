d20de00c29500237b8193f9e01650ee91f44be59
michael <michaelrantohi@gmail.com>
2025-05-06 20:45:33 -0400

    Implemented Healthcare GPO Compliance checker functionality, added menu options for industry specifiec compliance verification

--- src/Main.py
+++ src/Main.py
@@ -8,14 +8,17 @@ cc = ComplianceChecker()
 
 print("===[GPOGuard]===")
 while exit == False: # UI Continues until exit is True
-    print("\n=[MAIN MENU]=") # Main Menu options
-    print("[1] GPO Compliance Checker\n")
+    print("\n=[MAIN MENU]=")# Main Menu options
+    print("[1] Custom GPO Compliance")
+    print("[2] Healthcare GPO Compliance (HIPAA 164.312(b) + NIST 800‑53 AU‑2)")
+    print("[3] Finance GPO Compliance (PCI‑DSS 8.2.3 + NIST 800‑53 IA‑5)")
+    print("[4] Enterprise GPO Compliance (NIST 800-53)")
     choice = input("> ")
 
-    if choice == "1": # Main Menu option #1 GPO Compliance Checker
+    if choice == "1": # Main Menu option #1 Custom GPO Compliance
         post_result_choice = "1" # Users can keep scanning for compliance until 2 is entered in post menu
         while post_result_choice == "1":
-            print("\n=[GPO Compliance Checker]=")
+            print("\n=[Custom GPO Compliance Checker]=")
             print("[FILE SELECTION]")
             print("Please select a GPO file to check for compliance (/data directory)"
                   "\n\nFiles in /data directory:")
@@ -29,9 +32,28 @@ while exit == False: # UI Continues until exit is True
             cc.get_bl_settings_and_values(f"../data/{baseline_file}")
             cc.get_gpo_settings_and_values(f"../data/{gpo_file}")
 
-            print("\n[COMPLIANCE RESULTS]")
+            print("\n[CUSTOM GPO COMPLIANCE RESULTS]")
             cc.check_gpo_compliance()
             print("\n[MENU]") # Post-menu where user can break loop or scan another doc for compliance
+            print("[1] Check another file for compliance")
+            print("[2] Main Menu")
+            post_result_choice = input("> ")
+    if choice == "2":  # Main Menu option #2 Healthcare GPO Compliance Checker
+        post_result_choice = "1"
+        while post_result_choice == "1":
+            print("\n=[Healthcare GPO Compliance]=")
+            print("[FILE SELECTION]")
+            print("Please select a GPO file to check for compliance (/data directory)"
+                  "\n\nFiles in /data directory:")
+            print(list("../data"))  # Prints files in directory using list function
+            gpo_file = input("> ")
+
+            cc.get_bl_settings_and_values(f"../data/healthcare_baseline.csv")
+            cc.get_gpo_settings_and_values(f"../data/{gpo_file}")
+
+            print("\n[HEALTHCARE GPO COMPLIANCE RESULTS]")
+            cc.check_gpo_compliance()
+            print("\n[MENU]")  # Post-menu where user can break loop or scan another doc for compliance
             print("[1] Check another file for compliance")
             print("[2] Main Menu")
             post_result_choice = input("> ")
