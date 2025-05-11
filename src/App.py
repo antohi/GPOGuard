34dfb6f904950d4b8ff6f0ed8ac30227279047e4
michael <michaelrantohi@gmail.com>
2025-05-11 19:15:46 -0400

    Added columns to App.py to structure layout, designed Run Custom Selection button

--- src/App.py
+++ src/App.py
@@ -1,4 +1,18 @@
 import streamlit as app
 
-app.title("GPOGuard")
-gpo_file = app.text_input("Group Policy Export File:")
+app.set_page_config(page_title="GPOGuard", layout="centered")
+
+col1, col2, col3= app.columns(3)
+with col2:
+    app.markdown(f"## :blue[{"[GPO GUARD]"}]")
+
+col5, col4, col6 = app.columns(3)
+with col4:
+    app.markdown("### [SCAN SELECTION]")
+
+custom = app.button("🔍 Run Custom Scan", use_container_width=True)
+
+
+
+
+
