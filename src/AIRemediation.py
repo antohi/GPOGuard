be67c375c829114c329dcde66a1a45c75e815e19
michael <michaelrantohi@gmail.com>
2025-05-11 13:28:07 -0400

    Switched from OpenAI API to groq cloud API because I have no money

--- src/AIRemediation.py
+++ src/AIRemediation.py
@@ -1,11 +1,14 @@
 import os
 from openai import OpenAI
 from dotenv import load_dotenv
+from groq import Groq
+
 
 class AIRemediation:
 
     @staticmethod
     def get_ai_suggestions(control_id, description):
+        """
         load_dotenv()
         api_key = os.getenv("API_KEY")
         try:
@@ -23,6 +26,31 @@ class AIRemediation:
 
         except Exception as e:
             print(f"[!] AI ERROR: Could not get suggestion: {e}")
+
+        """
+        load_dotenv()
+        api_key = os.getenv("API_KEY")
+
+        client = Groq(
+            api_key=os.getenv("API_KEY"),
+        )
+
+        chat_completion = client.chat.completions.create(
+            messages=[
+                {
+                    "role": "user",
+                    "content": f"You are a cybersecurity compliance expert. Using 2-3 sentences "
+                               f"help the user fix their compliance issue."
+                               f"what is a GPO-based remediation for the following NIST 800-53 control? "
+                               f"{control_id}: {description}",
+                }
+            ],
+            model="llama-3.3-70b-versatile",
+            stream=False,
+        )
+
+        return chat_completion.choices[0].message.content
+
 
 
 
